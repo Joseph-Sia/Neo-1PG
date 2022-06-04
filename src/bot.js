1b0fca7013d9518a311979ed0bff2bea17b0da0e
Joseph-Sia <josephsia2006@gmail.com>
2022-06-04 14:40:42 +0800

    Added bot reply feature

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,24 @@
-import { Client, Intents } from 'discord.js'; 
+import { config } from 'dotenv';
+config({ path: '.env' })
+
+import { Client, Intents, Message } from 'discord.js';
+import { EndBehaviorType } from '@discordjs/voice';
+
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
 bot.on('ready', () => console.log('ready'));
-bot.login('OTM0Njc4MTY4MDAwOTQ2MjE3.Yezk4A.MgC0aWzeZ3V7qAdV5KjZRwsA0ck');
+bot.on('message', async(msg) => {
+    //if (msg.author.bot) return;
+
+    //await msg.reply('SUP')
+    if (msg.author.bot) {return}
+    if (msg.content.startsWith('apple')) {
+        msg.reply('iPhone')
+    }
+
+    if (msg.content.startsWith('!dm')) {
+        let messageContent=msg.content.replace('!dm', '')
+        msg.member.send('HELLO!!')
+    }
+})
+bot.login(process.env.BOT_TOKEN);
